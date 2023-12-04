101a5ae617a9c8893c5aaf326ecfe993d0627da6
John Keck <robertjkeck2@gmail.com>
2023-12-04 13:49:59 -0600

    finished day 1 java

--- java/app/src/main/java/com/java/aoc23/Day1.java
+++ java/app/src/main/java/com/java/aoc23/Day1.java
@@ -4,6 +4,8 @@
 package com.java.aoc23;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day1 implements Day {
     public void part1(String input) {
@@ -23,6 +25,49 @@ public class Day1 implements Day {
     }
 
     public void part2(String input) {
-        System.out.println("Part 2 not implemented yet");
+        String[] calibration_values = input.split("\n");
+        int sum = 0;
+        for (String calibration_value : calibration_values) {
+            int[] numbers = parse_digits(calibration_value);
+            int number = numbers[0] * 10 + numbers[numbers.length - 1];
+            sum += number;
+        }
+        System.out.println("Part 2: " + sum);
+    }
+
+    public int[] parse_digits(String input) {
+        Pattern re = Pattern.compile("(?=(one|two|three|four|five|six|seven|eight|nine|\\d))",
+                Pattern.CASE_INSENSITIVE);
+        Matcher m = re.matcher(input);
+        ArrayList<Integer> digits = new ArrayList<Integer>();
+        while (m.find()) {
+            String digit = m.group(1);
+            if (digit.equals("one")) {
+                digits.add(1);
+            } else if (digit.equals("two")) {
+                digits.add(2);
+            } else if (digit.equals("three")) {
+                digits.add(3);
+            } else if (digit.equals("four")) {
+                digits.add(4);
+            } else if (digit.equals("five")) {
+                digits.add(5);
+            } else if (digit.equals("six")) {
+                digits.add(6);
+            } else if (digit.equals("seven")) {
+                digits.add(7);
+            } else if (digit.equals("eight")) {
+                digits.add(8);
+            } else if (digit.equals("nine")) {
+                digits.add(9);
+            } else {
+                digits.add(Integer.parseInt(digit));
+            }
+        }
+        int[] numbers = new int[digits.size()];
+        for (int i = 0; i < digits.size(); i++) {
+            numbers[i] = digits.get(i);
+        }
+        return numbers;
     }
 }
