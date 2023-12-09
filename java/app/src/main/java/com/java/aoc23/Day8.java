5231f9413b75b1548ab06dcfd9add86f99de0692
John Keck <robertjkeck2@gmail.com>
2023-12-08 21:40:49 -0600

    day 8 part 2 wip

--- java/app/src/main/java/com/java/aoc23/Day8.java
+++ java/app/src/main/java/com/java/aoc23/Day8.java
@@ -43,6 +43,48 @@ public class Day8 implements Day {
     }
 
     public void part2(String input) {
-        System.out.println("Part 2: " + "TODO");
+        String[] lines = input.split("\n");
+        String[] instructions = lines[0].split("");
+        HashMap<String, String> map = new HashMap<String, String>();
+        for (int i = 0; i < lines.length; i++) {
+            if (i == 0 || i == 1) {
+                continue;
+            }
+            String[] line = lines[i].split(" = ");
+            String key = line[0];
+            String[] value = line[1].replace("(", "").replace(")", "").split(", ");
+
+            map.put(key + "-L", value[0]);
+            map.put(key + "-R", value[1]);
+        }
+        String[] locations = { "AAA", "HVA", "FXA", "LBA", "PSA", "GHA" };
+        boolean found = false;
+        long index = 0;
+        while (!found) {
+            String instruction = instructions[(int) (index % instructions.length)];
+            String[] newLocations = new String[locations.length];
+            for (int i = 0; i < locations.length; i++) {
+                String newLocation;
+                if (instruction.equals("L")) {
+                    newLocation = map.get(locations[i] + "-L");
+                } else {
+                    newLocation = map.get(locations[i] + "-R");
+                }
+                newLocations[i] = newLocation;
+            }
+            index++;
+            int numEndInZ = 0;
+            for (int i = 0; i < newLocations.length; i++) {
+                if (newLocations[i].endsWith("Z")) {
+                    numEndInZ++;
+                }
+            }
+            if (numEndInZ == newLocations.length) {
+                found = true;
+                break;
+            }
+            locations = newLocations.clone();
+        }
+        System.out.println("Part 2: " + index);
     }
 }
