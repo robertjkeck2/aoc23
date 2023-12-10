f38b1ef224efbbbf8eed033eb02c0d3348a1e975
John Keck <robertjkeck2@gmail.com>
2023-12-10 10:23:03 -0600

    day 10 part 2

--- java/app/src/main/java/com/java/aoc23/Day10.java
+++ java/app/src/main/java/com/java/aoc23/Day10.java
@@ -4,26 +4,34 @@
 package com.java.aoc23;
 
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.awt.Polygon;
 
 public class Day10 implements Day {
     public void part1(String input) {
         Puzzle puzzle = new Puzzle(input, "E");
         int pathLength = puzzle.solve();
-        System.out.println("Part 1: " + pathLength);
+        System.out.println("Part 1: " + pathLength / 2);
     }
 
     public void part2(String input) {
-        System.out.println("Part 2: " + "TODO");
+        Puzzle puzzle = new Puzzle(input, "E");
+        puzzle.solve();
+        int numberOfPointsInPolygon = puzzle.numberOfPointsInPolygon();
+        System.out.println("Part 2: " + numberOfPointsInPolygon);
     }
 }
 
 final class Puzzle {
     String[][] input;
+    ArrayList<int[]> visitedLocations = new ArrayList<int[]>();
+    Polygon polygon;
     int[] startLocation;
     int[] currentLocation;
     String currentDirection; // N, E, S, W
     boolean isFinished = false;
     int pathLength = 0;
+    int pointsInPolygon = 0;
 
     public Puzzle(String input, String startingDirection) {
         this.input = Arrays.stream(input.split("\n"))
@@ -38,7 +46,26 @@ final class Puzzle {
         while (!isFinished) {
             move();
         }
+        polygon = new Polygon(visitedLocations.stream().mapToInt(i -> i[0]).toArray(),
+                visitedLocations.stream().mapToInt(i -> i[1]).toArray(),
+                visitedLocations.size());
         return pathLength;
+    }
+
+    public int numberOfPointsInPolygon() {
+        for (int i = 0; i < input.length; i++) {
+            String[] row = input[i];
+            for (int j = 0; j < row.length; j++) {
+                if (isPointInPolygon(i, j)) {
+                    pointsInPolygon++;
+                }
+            }
+        }
+        return pointsInPolygon - (pathLength / 2) + 1;
+    }
+
+    private boolean isPointInPolygon(int x, int y) {
+        return polygon.contains(x, y);
     }
 
     private void findStartLocation() {
@@ -56,6 +83,7 @@ final class Puzzle {
 
     private void move() {
         String currentElement = input[currentLocation[0]][currentLocation[1]];
+        visitedLocations.add(new int[] { currentLocation[0], currentLocation[1] });
         if (currentElement.equals("|")) {
             pathLength++;
             if (currentDirection.equals("N")) {
