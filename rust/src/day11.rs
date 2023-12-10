d2848f61e8d375d8e8fb83a16258ba5654e2e0dd
John Keck <robertjkeck2@gmail.com>
2023-12-10 10:26:59 -0600

    day 11 setup

---
+++ rust/src/day11.rs
@@ -0,0 +1,18 @@
+/*
+ * Advent of Code 2023 Day 11
+ */
+use crate::day::Day;
+
+pub struct Day11;
+
+impl Day for Day11 {
+    fn part1(&self, input: String) {
+        let _: Vec<&str> = input.split("\n").collect();
+        println!("Part 1: {}", "TODO");
+    }
+
+    fn part2(&self, input: String) {
+        let _: Vec<&str> = input.split("\n").collect();
+        println!("Part 2: {}", "TODO");
+    }
+}
