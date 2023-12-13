eb5077fc622059749a2f0973037de876a8128cda
John Keck <robertjkeck2@gmail.com>
2023-12-13 17:24:42 -0600

    day 12 part 2 wip

--- java/app/src/main/java/com/java/aoc23/Day12.java
+++ java/app/src/main/java/com/java/aoc23/Day12.java
@@ -3,20 +3,28 @@
  */
 package com.java.aoc23;
 
+import java.util.HashMap;
+
+import com.java.aoc23.SpringRow.RowPermutation;
+
 public class Day12 implements Day {
     public void part1(String input) {
         SpringReport springReport = new SpringReport(input);
-        int sum = springReport.getNumPermutations();
+        long sum = springReport.getNumPermutations(false);
         System.out.println("Part 1: " + sum);
     }
 
     public void part2(String input) {
-        System.out.println("Part 2: " + "TODO");
+        SpringReport springReport = new SpringReport(input);
+        springReport.getNumPermutations(false);
+        long sum = springReport.getNumPermutations(true);
+        System.out.println("Part 2: " + sum);
     }
 }
 
 final class SpringReport {
     private SpringRow[] springRows;
+    private static final HashMap<RowPermutation, Integer> memoMap = new HashMap<>();
 
     public SpringReport(String input) {
         String[] rows = input.split("\n");
@@ -30,10 +38,12 @@ final class SpringReport {
         return springRows;
     }
 
-    public int getNumPermutations() {
-        int sum = 0;
+    public long getNumPermutations(boolean expanded) {
+        long sum = 0;
         for (SpringRow springRow : springRows) {
-            sum += springRow.getRowPermutations(springRow.getRow(), springRow.getRowGroups());
+            sum += springRow.getRowPermutations(expanded ? springRow.getExpandedRow() : springRow.getRow(),
+                    expanded ? springRow.getExpandedRowGroups() : springRow.getRowGroups(),
+                    expanded ? new HashMap<RowPermutation, Integer>() : memoMap);
         }
         return sum;
 
@@ -56,11 +66,38 @@ final class SpringRow {
         return rowGroupsInt;
     }
 
+    public int[] getExpandedRowGroups() {
+        int[] rowGroups = getRowGroups();
+        int[] expandedRowGroups = new int[rowGroups.length * 5];
+        for (int i = 0; i < expandedRowGroups.length; i++) {
+            expandedRowGroups[i] = rowGroups[i % rowGroups.length];
+        }
+        return expandedRowGroups;
+    }
+
     public String getRow() {
         return row.split(" ")[0];
     }
 
-    public int getRowPermutations(String rowString, int[] rowGroups) {
+    public String getExpandedRow() {
+        String expandedRow = "";
+        String rowSplit = row.split(" ")[0];
+        for (int i = 0; i < 5; i++) {
+            expandedRow += rowSplit;
+            expandedRow += i == 4 ? "" : "?";
+        }
+        return expandedRow;
+    }
+
+    public record RowPermutation(String row, int[] rowGroups) {
+    }
+
+    public int getRowPermutations(String rowString, int[] rowGroups, HashMap<RowPermutation, Integer> memoMap) {
+        RowPermutation rowPermutation = new RowPermutation(rowString, rowGroups);
+        if (memoMap.containsKey(rowPermutation)) {
+            return memoMap.get(rowPermutation);
+        }
+
         if (rowString.isBlank()) {
             if (rowGroups.length == 0) {
                 return 1;
@@ -71,10 +108,10 @@ final class SpringRow {
 
         int permutations = 0;
         if (rowString.startsWith(".")) {
-            permutations += getRowPermutations(rowString.substring(1), rowGroups);
+            permutations += getRowPermutations(rowString.substring(1), rowGroups, memoMap);
         } else if (rowString.startsWith("?")) {
-            permutations += getRowPermutations("." + rowString.substring(1), rowGroups)
-                    + getRowPermutations("#" + rowString.substring(1), rowGroups);
+            permutations += getRowPermutations("." + rowString.substring(1), rowGroups, memoMap)
+                    + getRowPermutations("#" + rowString.substring(1), rowGroups, memoMap);
         } else {
             if (rowGroups.length > 0) {
                 int damaged = rowGroups[0];
@@ -87,14 +124,15 @@ final class SpringRow {
                     if (damaged == rowString.length()) {
                         permutations = newGroups.length == 0 ? 1 : 0;
                     } else if (rowString.charAt(damaged) == '.') {
-                        permutations = getRowPermutations(rowString.substring(damaged + 1), newGroups);
+                        permutations = getRowPermutations(rowString.substring(damaged + 1), newGroups, memoMap);
                     } else if (rowString.charAt(damaged) == '?') {
-                        permutations = getRowPermutations("." + rowString.substring(damaged + 1), newGroups);
+                        permutations = getRowPermutations("." + rowString.substring(damaged + 1), newGroups, memoMap);
                     }
                 }
             }
         }
 
+        memoMap.put(rowPermutation, permutations);
         return permutations;
     }
 }
