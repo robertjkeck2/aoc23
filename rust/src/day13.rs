d81288265591d620d7b719a3ff7dd6ec7e6f0a42
John Keck <robertjkeck2@gmail.com>
2023-12-13 21:58:15 -0600

    wip

--- rust/src/day13.rs
+++ rust/src/day13.rs
@@ -7,12 +7,27 @@ pub struct Day13;
 
 impl Day for Day13 {
     fn part1(&self, input: String) {
-        let _: Vec<&str> = input.split("\n").collect();
+        let reflections: Vec<&str> = input.split("\n\n").collect();
+        for reflection in reflections {
+            let reflection: Reflection = Reflection::new(reflection.to_string());
+            println!("{:?}", reflection.rows[0]);
+        }
         println!("Part 1: {}", "TODO");
     }
 
     fn part2(&self, input: String) {
         let _: Vec<&str> = input.split("\n").collect();
         println!("Part 2: {}", "TODO");
+    }
+}
+
+pub struct Reflection {
+    rows: Vec<Vec<char>>,
+}
+
+impl Reflection {
+    fn new(input: String) -> Reflection {
+        let rows: Vec<Vec<char>> = input.split("\n").map(|row| row.chars().collect()).collect();
+        Reflection { rows }
     }
 }
