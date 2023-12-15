9a9be27c6d4b86677e4a6de5f01c9fe6806cdda8
John Keck <robertjkeck2@gmail.com>
2023-12-15 00:35:30 -0600

    day 15 part 1

--- rust/src/day15.rs
+++ rust/src/day15.rs
@@ -7,12 +7,32 @@ pub struct Day15;
 
 impl Day for Day15 {
     fn part1(&self, input: String) {
-        let _: Vec<&str> = input.split("\n").collect();
-        println!("Part 1: {}", "TODO");
+        let values: Vec<&str> = input.split(",").collect();
+        let mut sum: i32 = 0;
+        for value in values {
+            let hash = Hash::new(value.to_string());
+            sum += hash.value;
+        }
+        println!("Part 1: {}", sum);
     }
 
     fn part2(&self, input: String) {
         let _: Vec<&str> = input.split("\n").collect();
         println!("Part 2: {}", "TODO");
+    }
+}
+
+pub struct Hash {
+    value: i32,
+}
+
+impl Hash {
+    pub fn new(input: String) -> Hash {
+        let mut value: i32 = 0;
+        for c in input.chars() {
+            let new_val: i32 = value + c as i32;
+            value = new_val * 17 % 256;
+        }
+        Hash { value }
     }
 }
